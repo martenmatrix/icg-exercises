effcaba26fd56d88317137b91cb84f903c95f26e
Marten <derapfelesser@aol.de>
2023-11-06 15:21:13 +0100

    add hausaufgaben

---
+++ exercise-2/Hausaufgaben (Baum + Wolke)/main.js
@@ -0,0 +1,66 @@
+let gl;
+let program;
+let posVBO,
+	indVBO,
+	colorVBO;
+
+function main() {
+	const canvas = document.getElementById("gl-canvas");
+	gl = canvas.getContext('webgl2');
+
+	gl.viewport(0,0,canvas.width,canvas.height);
+	gl.clearColor(1.0,1.0,1.0,1.0);
+
+	gl.enable(gl.DEPTH_TEST);
+
+	program = initShaders(gl, "vertex-shader", "fragment-shader");
+	gl.useProgram(program);
+
+	initBuffers();
+	render();
+}
+
+function initBuffers() {
+	posVBO = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, posVBO);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tree.positions), gl.STATIC_DRAW);
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+	indVBO = gl.createBuffer()
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indVBO)
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(tree.indices), gl.STATIC_DRAW)
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
+
+
+	/*
+	colorVBO = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorVBO);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+	*/
+}
+
+function render() {
+	gl.bindBuffer(gl.ARRAY_BUFFER, posVBO);
+	const posLoc = gl.getAttribLocation(program, "vPosition");
+	gl.enableVertexAttribArray(posLoc);
+	gl.vertexAttribPointer(posLoc, 3, gl.FLOAT, false, 0, 0);
+
+	// Link data in VBO to shader variables
+	/*
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorVBO);
+	const colorLoc = gl.getAttribLocation(program, "vColor");
+	gl.enableVertexAttribArray(colorLoc);
+	gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+	*/
+
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indVBO);
+
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_INT, 0);
+}
+
+window.onload = function () {
+	main();
+};
+
